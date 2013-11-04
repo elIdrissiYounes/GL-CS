8a396e8b8037e1c84483c0e2832f52aae1b14501
Nick Gravelyn <nickgravelyn@gmail.com>
2013-11-03 23:18:40 -0800

    Initial commit of baseline spec parsing and code-gen to create classes for each version/profile with enums and function pointers (as delegate member variables).

---
+++ GL-CS/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GL-CS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GL-CS")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("40333d02-052d-41fe-861e-cc196bd80695")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ GLCSGen/CodeWriter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+
+namespace GLCSGen
+{
+    class CodeWriter : IDisposable
+    {
+        private readonly StreamWriter writer;
+
+        private int tabCount = 0;
+        private string tabString = "";
+
+        public int TabCount
+        {
+            get { return tabCount; }
+            set
+            {
+                tabCount = Math.Max(value, 0);
+                tabString = new string(' ', tabCount * 4);
+            }
+        }
+
+        public CodeWriter(string filename)
+        {
+            writer = new StreamWriter(filename);
+        }
+
+        ~CodeWriter()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            writer.Dispose();
+        }
+
+        public void WriteLine()
+        {
+            writer.WriteLine();
+        }
+
+        public void WriteLine(string line)
+        {
+            writer.Write(tabString);
+            writer.WriteLine(line);
+        }
+
+        public void WriteLine(string format, params object[] args)
+        {
+            writer.Write(tabString);
+            writer.WriteLine(format, args);
+        }
+
+        public void WriteOpenBrace()
+        {
+            WriteLine("{");
+            TabCount++;
+        }
+
+        public void WriteCloseBrace()
+        {
+            TabCount--;
+            WriteLine("}");
+        }
+    }
+}
