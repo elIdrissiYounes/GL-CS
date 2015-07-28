9d9daf6a5a352bfd723ae0e91c8d57e72fcc9227
Nick Gravelyn <nick@gravelyn.com>
2015-07-28 07:39:48 -0700

    Adding TODOs for options I'd like to explore later on.

--- GLCSGen/CommandLineOptions.cs
+++ GLCSGen/CommandLineOptions.cs
@@ -9,5 +9,9 @@ namespace GLCSGen
 
         [Option('o', "outdir", Required = false, HelpText = "Path to directory where generated files are written.")]
         public string OutputPath { get; set; } = "output";
+
+        // TODO: Add option to only generate certain versions/profiles
+        // TODO: Add option to add more C#-like methods (rewrite out parameters as return values, etc)
+        // TODO: Add option to only include specific functions (and only include enums that are in groups used by those function parameters).
     }
 }
